14102f1d9a30691841c289cd2d3670f6909a5eec
JunichiHattori <junhat6@gmail.com>
2026-04-23 01:56:38 +0900

    tailwindcss導入 #32

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import ViteFonts from 'unplugin-fonts/vite'
@@ -8,6 +9,7 @@ import ViteFonts from 'unplugin-fonts/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vue(),
     vueDevTools(),
     ViteFonts({
