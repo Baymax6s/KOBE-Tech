ef76a8c44ca484bb2bdf9927b13fa23854ee549d
JunichiHattori <junhat6@gmail.com>
2026-04-26 10:02:14 +0900

    ログアウトボタンの表示条件を認証状態に基づくように変更 #26

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -2,22 +2,21 @@
   <v-app-bar app color="primary" dark>
     <v-img src="/kdtech-icon.png" alt="Logo" max-width="200" class="mr-4" />
     <v-spacer></v-spacer>
-    <v-btn v-if="showLogout" @click="logout" color="white"> ログアウト </v-btn>
+    <v-btn v-if="auth.isAuthenticated" @click="logout" color="white">
+      ログアウト
+    </v-btn>
   </v-app-bar>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 
-const emit = defineEmits<{
-  (e: 'onLogout'): void
-}>()
-
-const route = useRoute()
-const showLogout = computed(() => route.path !== '/login')
+const router = useRouter()
+const auth = useAuthStore()
 
 const logout = () => {
-  emit('onLogout')
+  auth.clearToken()
+  router.push('/')
 }
 </script>
