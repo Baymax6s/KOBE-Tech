5aa6502375fe3b39fedc889937ed52f13b282d84
taipeitaipei <kd1369856@st.kobedenshi.ac.jp>
2026-05-13 11:02:22 +0900

    設定ボタンのプロフィール、パスワード変更ボタンを追加

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -5,7 +5,11 @@ import { useAuthStore } from '@/stores/auth'
 const router = useRouter()
 const auth = useAuthStore()
 
-const goToSettings = () => {
+const goToProfile = () => {
+  router.push('/profile')
+}
+
+const goToPasswordSettings = () => {
   router.push('/settings/password')
 }
 
@@ -18,21 +22,67 @@ const goToLogin = () => {
   router.push('/login')
 }
 </script>
-
 <template>
   <v-app-bar app color="primary" dark>
-    <v-img src="/kdtech-icon.png" alt="Logo" max-width="200" class="mr-4" />
+    <v-img
+      src="/kdtech-icon.png"
+      alt="Logo"
+      max-width="200"
+      class="mr-4"
+    />
+
     <v-spacer></v-spacer>
+
+    <!-- 設定メニュー -->
+    <v-menu offset-y>
+      <template #activator="{ props }">
+        <v-btn
+          v-if="auth.isAuthenticated"
+          icon="mdi-cog"
+          aria-label="設定"
+          v-bind="props"
+        />
+      </template>
+
+      <v-list>
+        <v-list-item @click="goToProfile">
+          <template #prepend>
+            <v-icon>mdi-account</v-icon>
+          </template>
+
+          <v-list-item-title>
+            プロフィール
+          </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item @click="goToPasswordSettings">
+          <template #prepend>
+            <v-icon>mdi-lock</v-icon>
+          </template>
+
+          <v-list-item-title>
+            パスワード変更
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <!-- ログアウト -->
     <v-btn
       v-if="auth.isAuthenticated"
-      icon="mdi-cog"
-      aria-label="設定"
-      @click="goToSettings"
-    />
-    <v-btn v-if="auth.isAuthenticated" @click="logout" color="white">
+      @click="logout"
+      color="white"
+    >
       ログアウト
     </v-btn>
-    <v-btn v-else @click="goToLogin" color="white" variant="outlined">
+
+    <!-- ログイン -->
+    <v-btn
+      v-else
+      @click="goToLogin"
+      color="white"
+      variant="outlined"
+    >
       ログイン
     </v-btn>
   </v-app-bar>
